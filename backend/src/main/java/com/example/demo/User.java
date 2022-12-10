8ffcdf9b9dcca1d18f0caccf1185cb2b3c62c33e
NeuzaSousa <neuzarssousa@gmail.com>
2022-12-10 15:13:56 +0100

    First commit

---
+++ backend/src/main/java/com/example/demo/User.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="User")
+public class User {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	int id;
+	String name;
+	String firstname;
+	String email;
+	String typeuser;
+	
+	public String getTypeuser() {
+		return typeuser;
+	}
+	public void setTypeuser(String typeuser) {
+		this.typeuser = typeuser;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
---
+++ backend/src/main/java/com/example/demo/temp.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+/*package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="TypeUser")
+public class TypeUser {
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	int id;
+	String typeuser;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTypeuser() {
+		return typeuser;
+	}
+	public void setTypeuser(String typeuser) {
+		this.typeuser = typeuser;
+	}
+}*/
