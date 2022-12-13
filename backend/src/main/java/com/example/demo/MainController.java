d4e14c51001a82c9b3ad182e5798e526f8547dc2
NeuzaSousa <neuzarssousa@gmail.com>
2022-12-13 17:05:48 +0100

    delete user and get user by id are working in the backend

--- backend/src/main/java/com/example/demo/MainController.java
+++ backend/src/main/java/com/example/demo/MainController.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,6 +29,17 @@ public class MainController {
 	@GetMapping("/users")
 	public @ResponseBody Iterable<User> getAllUsers() {
 		return userRepository.findAll();
+	}
+	
+	@GetMapping("/users/{id}")
+	public @ResponseBody Optional<User> getUserById(@PathVariable int id){
+		return userRepository.findById(id);
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		userRepository.deleteById(id);
+		//getAllUsers();
 	}
 	
 	@PostMapping("/types")
